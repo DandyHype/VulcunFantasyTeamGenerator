bf82da0b42ade28686348aa47dd835dac717ef2a
DanDyHype <gyr919@gmail.com>
2015-09-12 14:14:34 +0200

    removed some commented expressions

--- src/FantasyTeam.java
+++ src/FantasyTeam.java
@@ -125,8 +125,6 @@ public class FantasyTeam {
                         remainingPlayers.remove(usedPlayer);
 
                     addFlexPicks(remainingPlayers, fantasyTeam);
-                    //printTeam();
-                    //System.out.print(remainingPlayers);
                     remainingPlayers.clear();
 
                 }
